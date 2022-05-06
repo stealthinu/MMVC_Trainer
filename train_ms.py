7f6c21367f09b33d61e4facd599b85bbb18b2475
Satoh Kiyoshi <satoh@hakuba.jp>
2022-05-06 19:50:39 +0900

    for Windows WSL2

--- train_ms.py
+++ train_ms.py
@@ -11,6 +11,7 @@ from torch.utils.tensorboard import SummaryWriter
 import torch.multiprocessing as mp
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn.parallel import DataParallel as DP
 from torch.cuda.amp import autocast, GradScaler
 import datetime
 import pytz
@@ -62,7 +63,7 @@ def run(rank, n_gpus, hps):
     writer = SummaryWriter(log_dir=hps.model_dir)
     writer_eval = SummaryWriter(log_dir=os.path.join(hps.model_dir, "eval"))
 
-  dist.init_process_group(backend='nccl', init_method='env://', world_size=n_gpus, rank=rank)
+#  dist.init_process_group(backend='nccl', init_method='env://', world_size=n_gpus, rank=rank)
   torch.manual_seed(hps.train.seed)
   torch.cuda.set_device(rank)
   train_dataset = TextAudioSpeakerLoader(hps.data.training_files, hps.data)
@@ -98,17 +99,19 @@ def run(rank, n_gpus, hps):
       hps.train.learning_rate, 
       betas=hps.train.betas, 
       eps=hps.train.eps)
-  net_g = DDP(net_g, device_ids=[rank])
-  net_d = DDP(net_d, device_ids=[rank])
+#  net_g = DDP(net_g, device_ids=[rank])
+#  net_d = DDP(net_d, device_ids=[rank])
+  net_g = DP(net_g, device_ids=[rank])
+  net_d = DP(net_d, device_ids=[rank])
 
   logger.info('FineTuning : '+str(hps.fine_flag))
   if hps.fine_flag:
-      logger.info('Load model : '+str(hps.fine_model_g))
-      logger.info('Load model : '+str(hps.fine_model_d))
-      _, _, _, epoch_str = utils.load_checkpoint(hps.fine_model_g, net_g, optim_g)
-      _, _, _, epoch_str = utils.load_checkpoint(hps.fine_model_d, net_d, optim_d)
-      epoch_str = 1
-      global_step = 0
+    logger.info('Load model : '+str(hps.fine_model_g))
+    logger.info('Load model : '+str(hps.fine_model_d))
+    _, _, _, epoch_str = utils.load_checkpoint(hps.fine_model_g, net_g, optim_g)
+    _, _, _, epoch_str = utils.load_checkpoint(hps.fine_model_d, net_d, optim_d)
+    epoch_str = 1
+    global_step = 0
 
   else:
     try:
